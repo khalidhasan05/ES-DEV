9dd00b709863057b07409c65d2545618ef301fc7
KHALID HASAN <khalidhasanpranta05@gmail.com>
2025-02-08 03:46:04 +0600

    function practice commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    println("hello Android")
+  val name = "Khalid Hasan"
+    val age = 23
+    val height=5.5
+    printDetails(name, age, height)
+}
+
+fun printDetails(name: String, age: Int, height: Double){
+    println(name)
+    println("Age: $age")
+    println("Height: $height")
 }
