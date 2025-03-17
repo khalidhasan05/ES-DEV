b0118832f622b0d4784412ead17ae770a492ba07
KHALID HASAN <khalidhasanpranta05@gmail.com>
2025-03-17 08:38:46 +0600

    queue

--- src/Main.kt
+++ src/Main.kt
@@ -35,6 +35,6 @@ fun main(){
    // qn05()
     //draft()
    // nullSafe()
-    collection()
-
+   // collection()
+    queue()
 }
--- src/draft.kt
+++ src/draft.kt
@@ -1,10 +1,41 @@
-fun draft(){
-    var  age:Int? = null
-   // age = 20
-    info(age?:10)
+fun queue() {
+ val fruits = MyQueue<String>()
+ fruits.enqueue("Lichi")
+ fruits.enqueue("Orange")
+ val fruitList = listOf("Banana", "Apple", "Mango")
+ fruits.enqueueList(fruitList)
+ fruits.dequeue()
+ fruits.displayQueue()
 }
 
+class MyQueue<T> {
+ private val data = mutableListOf<T>()
 
-fun info(a:Int? = 0){
-    println(a)
+ fun enqueue(item: T) {
+  println("$item added to queue.")
+  data.add(item)
+ }
+
+ fun enqueueList(list: List<T>) {
+  list.forEach { enqueue(it) }
+ }
+
+ fun dequeue(): T? {
+  return if (data.isNotEmpty()) {
+   data.removeFirst()
+  } else {
+   null
+  }
+ }
+
+ fun displayQueue() {
+  if (data.isNotEmpty()) {
+   data.forEachIndexed { index, item ->
+    println("${index + 1}. $item")
+   }
+  } else {
+   println("There is no data.")
+  }
+ }
 }
+
